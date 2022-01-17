6e604cabd96ce430c1b9238ef31c5d58781a7b53
Camilo Mezú Mina <camilo.mezu@correounivalle.edu.co>
2022-01-16 20:17:51 -0500

    banner

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header
+          :navLinks="[
+        {
+          name: 'Home',
+          link: '/home',
+          dropdown: false,
+        },
+        { name: 'About', link: '/about', dropdown: false },
+        {
+          name: 'Contacts',
+          link: '/contact',
+          dropdown: false,
+        },
+        {
+          name: 'Projects',
+          link: '/projects',
+          dropdown: false,
+        },
+      ]"></Header>
   <router-view/>
 </template>
+
+<script >
+import Header from "@/components/Header";
+export default {
+  components: {Header}
+}
+
+</script>
 
 <style>
 #app {
@@ -15,16 +38,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
